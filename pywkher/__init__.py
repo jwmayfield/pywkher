a88aa0125c1dea25e2052a03433e67a76ee86943
Jason Mayfield <jason@codetalk.rs>
2013-05-25 19:40:23 +0000

    Don't automatically delete temporary files

--- pywkher/__init__.py
+++ pywkher/__init__.py
@@ -16,18 +16,16 @@ def generate_pdf(html='', url=''):
     wkhtmltopdf_cmd = environ.get('WKHTMLTOPDF_CMD', wkhtmltopdf_default)
 
     # Set up return file
-    pdf_file = NamedTemporaryFile(suffix='.pdf')
+    pdf_file = NamedTemporaryFile(delete=False, suffix='.pdf')
 
     if html:
         # Save the HTML to a temp file
-        html_file = NamedTemporaryFile(suffix='.html')
+        html_file = NamedTemporaryFile(delete=False, suffix='.html')
         html_file.write(html)
+        html_file.close()
 
         # wkhtmltopdf
         call_subprocess([wkhtmltopdf_cmd, '-q', html_file.name, pdf_file.name])
-
-        # Clean up
-        html_file.close()
     else:
         # wkhtmltopdf, using URL
         call_subprocess([wkhtmltopdf_cmd, '-q', url, pdf_file.name])
