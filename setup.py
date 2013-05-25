6e4c3ae259cd1a7b93e254f39a6b1b0b03841522
Jason Mayfield <jason@codetalk.rs>
2013-05-25 13:59:05 +0000

    Create package manifest, version 1.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pywkher',
-    version='1.0.2',
+    version='1.0.3',
     url='https://github.com/codetalkrs/pywkher',
     author='Jason Mayfield',
     author_email='jason@codetalk.rs',
